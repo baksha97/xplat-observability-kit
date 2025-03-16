4c78b496ca511d55ae7b75c4633f7f5e103e11c6
Alfayed Baksh <baksha97@gmail.com>
2025-03-16 19:58:11 -0400

    cleanup deps

--- jvm/app/build.gradle.kts
+++ jvm/app/build.gradle.kts
@@ -5,15 +5,11 @@ plugins {
 
 dependencies {
     implementation(project(":core"))
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.1")
-//    implementation("io.opentelemetry.android:android-agent:0.10.0-alpha")
-    implementation("io.opentelemetry:opentelemetry-sdk:1.48.0")
-    implementation("io.opentelemetry:opentelemetry-extension-kotlin:1.48.0")
-    implementation("io.opentelemetry:opentelemetry-sdk-extension-autoconfigure:1.48.0")
-    implementation(libs.opentelemetry.api.incubator)
+    implementation(libs.kotlinx.coroutines)
+    implementation(libs.opentelemetry.sdk)
     implementation(libs.opentelemetry.exporter.otlp)
-    implementation("io.opentelemetry:opentelemetry-exporter-jaeger:1.25.0")
-    implementation("io.opentelemetry:opentelemetry-semconv:1.25.0-alpha")
+    implementation(libs.opentelemetry.exporter.jaeger)
+    implementation(libs.opentelemetry.semconv)
     ksp(project(":core"))
 }
 
--- jvm/core/build.gradle.kts
+++ jvm/core/build.gradle.kts
@@ -1,5 +1,3 @@
-import org.jetbrains.kotlin.gradle.dsl.ExplicitApiMode
-
 plugins {
     alias(libs.plugins.kotlin.jvm)
     // Needed for test - otherwise wouldn't be needed.
@@ -10,20 +8,13 @@ dependencies {
     implementation(libs.kotlin.stdlib)
     implementation(libs.ksp.symbol.processing.api)
     implementation(libs.kotlin.poet)
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.1")
-//    implementation("io.opentelemetry.android:android-agent:0.10.0-alpha")
-    implementation("io.opentelemetry:opentelemetry-sdk:1.48.0")
-    implementation("io.opentelemetry:opentelemetry-extension-kotlin:1.48.0")
-
-    implementation(libs.opentelemetry.api.incubator)
-    implementation(libs.opentelemetry.exporter.otlp)
-    implementation("io.opentelemetry:opentelemetry-exporter-jaeger:1.25.0")
-    implementation("io.opentelemetry:opentelemetry-semconv:1.25.0-alpha")
-
+    implementation(libs.kotlinx.coroutines)
+    implementation(libs.opentelemetry.kotlin)
     kspTest(project(":core"))
     testImplementation(libs.mockk)
     testImplementation(libs.kotlin.test)
     testImplementation(libs.kotlinx.coroutines.test)
+    testImplementation(libs.opentelemetry.sdk)
 }
 
 kotlin {
